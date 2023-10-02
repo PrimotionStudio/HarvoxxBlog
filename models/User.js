9bdb5448bc24fca24688c0ff27b4870182738573
The Primotion Studio <oyedelenewton@gmail.com>
2023-10-02 11:35:08 +0100

    First Commit

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "admin"],
+    required: true,
+  },
+});
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
