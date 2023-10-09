6a7240b2e7cc169fa49efaa83cb0b1bbddb19ec3
The Primotion Studio <oyedelenewton@gmail.com>
2023-10-09 14:36:19 +0100

    fe and be

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -1,121 +1,101 @@
 const Post = require("./../models/Post");
+const session = require("express-session");
+let msg = "";
 
-let msg="";
-
-exports.getPosts = async (req, res, next) => {
+exports.getPosts = async () => {
   const posts = await Post.find();
-  return res.status(200).json({
-    status: "success",
-    result: posts,
-  });
+  return posts;
 };
 
-exports.getPost = async (req, res, next) => {
+exports.getPost = async () => {
   const post = await Post.findById(req.params.post_id);
-  return res.status(200).json({
-    status: "success",
-    result: post,
-  });
+  return post;
 };
 
 exports.getPostsByAuthor = async (req, res, next) => {
-  const post = await Post.find({author: req.body.author});
-  return res.status(200).json({
-    status: "success",
-    result: post,
-  });
+  const post = await Post.find({ author: req.body.author });
+  return post;
 };
 
 exports.get = async (req, res, next) => {
-  const post = await Post.find({author: req.body.author});
+  const post = await Post.find({ author: req.body.author });
+  return post;
+};
+
+exports.createPost = async (req, res, next) => {
+  let post = new Post({
+    body: req.body.body,
+    title: req.body.title,
+    tags: req.body.tags,
+    author: session.user._id,
+  });
+
+  post.save().then((err, doc) => {
+    if (err) {
+      console.log("Error occured: " + err);
+      msg = "Error occured: " + err;
+    } else {
+      msg = "posted";
+    }
+  });
+
   return res.status(200).json({
     status: "success",
-    result: post,
+    result: msg,
   });
 };
 
-exports.createPost = async (req,res,next) =>{
-  let post=new Post({
-    body:req.body.body,
-    title:req.body.title,
-    tags:req.body.tags,
-     author:req.params.id,
-  })
-
-  post.save()
-  .then((err,doc)=>{
-    if(err){
-      console.log("Error occured: "+err);
-      msg="Error occured: "+err
-    }
-    else{
-      msg="posted"
-    }
-  })
-
+exports.deletePost = async (req, res, next) => {
+  let author = req.params.id;
+  let id = req.body.id;
+  let post = await Post.findOne({ _id: id });
+  if (post.author._id == author) {
+    Post.deleteOne({ _id: post.id }).then(() => {
+      console.log(`Deleted post with following id: ${id}`);
+      msg = "Deleted";
+    });
+  }
   return res.status(200).json({
     status: "success",
-    result:msg,
-  })
-}
-
-exports.deletePost=async (req,res, next) =>{
-  let author=req.params.id;
-  let id=req.body.id
-  let post=await Post.findOne({_id: id});
-  if(post.author._id==author){
-    Post.deleteOne({_id:post.id})
-    .then(()=>{
-      console.log(`Deleted post with following id: ${id}`);
-      msg="Deleted";
-    })
-  }
-   return res.status(200).json({
-    status: "success",
-    result:msg,
-  })
-}
-
-exports.editPost=async (req,res,next) =>{
-  let author=req.params.id;
-  let id=req.body.id
-  let post=await Post.findOne({_id: id});
-  let body=req.body.body==undefined? post.body:req.body.body
-  let title=req.body.title==undefined? post.title:req.body.title
-  if(post.author._id==author){
-    post.title=title;
-    post.body=body;
-    post.save()
-    .then(()=>{
-    console.log(`Edited post with following id: ${id}`);
-      msg="Edited";
-    })
-  }
-   return res.status(200).json({
-    status: "success",
-    result:msg,
-  })
-}
-
-exports.likePost=async (req,res,next) =>{
-  let postid=req.body.post;
-  let post=await Post.findById(postid);
-  post.like()
-  .then(()=>{
-    msg="liked";
+    result: msg,
   });
-}
+};
 
-exports.dislikePost=async (req,res,next) =>{
-  let postid=req.body.post;
-  let post=await Post.findById(postid);
-  post.dislike()
-  .then(()=>{
-    msg="disliked";
+exports.editPost = async (req, res, next) => {
+  let author = req.params.id;
+  let id = req.body.id;
+  let post = await Post.findOne({ _id: id });
+  let body = req.body.body == undefined ? post.body : req.body.body;
+  let title = req.body.title == undefined ? post.title : req.body.title;
+  if (post.author._id == author) {
+    post.title = title;
+    post.body = body;
+    post.save().then(() => {
+      console.log(`Edited post with following id: ${id}`);
+      msg = "Edited";
+    });
+  }
+  return res.status(200).json({
+    status: "success",
+    result: msg,
   });
-}
+};
 
+exports.likePost = async (req, res, next) => {
+  let postid = req.body.post;
+  let post = await Post.findById(postid);
+  post.like().then(() => {
+    msg = "liked";
+  });
+};
 
+exports.dislikePost = async (req, res, next) => {
+  let postid = req.body.post;
+  let post = await Post.findById(postid);
+  post.dislike().then(() => {
+    msg = "disliked";
+  });
+};
 
 /*
   function of posts:
--- routes.js
+++ routes.js
@@ -1,20 +1,32 @@
 const express = require("express");
+const session = require("express-session");
 const Router = express.Router();
 const postCtrl = require("./controllers/postCtrl");
 const authCtrl = require("./controllers/authCtrl");
 const userCtrl = require("./controllers/userCtrl");
+const User = require("./models/User");
 
 const multer = require("multer");
-const multerupload=multer({
+const multerupload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cd) => {
       cd(null, "public/profiles");
     },
     filename: (req, file, cb) => {
-      
-      cb(null, `${req.userid}at${Date.now()}.jpg`)
-    }
-  })
+      cb(null, `${req.userid}at${Date.now()}.jpg`);
+    },
+  }),
+});
+
+// Home
+Router.route("/").get(async (req, res, next) => {
+  if (session.user && (await User.findOne({ email: session.user.email }))) {
+    return res.status(200).redirect("/home");
+  }
+  res.status(200).render("index", { posts: postCtrl.getPosts(), login: false });
+});
+Router.route("/home").get(authCtrl.isLoggedIn, (req, res, next) => {
+  res.status(200).render("index", { posts: postCtrl.getPosts(), login: true });
 });
 
 // Auth
@@ -25,19 +37,30 @@ Router.route("/signup")
   .post(authCtrl.signUp);
 Router.route("/signin")
   .get((req, res, next) => {
-    res.status(200).render("signin");
+    let alert;
+    if (session.alert) {
+      alert = session.alert;
+      session.alert = undefined;
+    }
+    res.status(200).render("signin", { alert: alert || "" });
   })
   .post(authCtrl.signIn);
-Router.route("/signout").post(authCtrl.isLoggedIn, authCtrl.signOut);
+Router.route("/signout")
+  .post(authCtrl.isLoggedIn, authCtrl.signOut)
+  .get(authCtrl.isLoggedIn, authCtrl.signOut);
 
 // Profile
 Router.route("/profile").get(authCtrl.isLoggedIn, userCtrl.getMe);
 
 // Update Profile Pic
-Router.post("/profilepic", multerupload.single("photo"), userCtrl.uploadProfilePic)
+Router.post(
+  "/profilepic",
+  multerupload.single("photo"),
+  userCtrl.uploadProfilePic
+);
 // Post
 Router.route("/post")
-  .post(authCtrl.isLoggedIn)
+  .post(authCtrl.isLoggedIn, postCtrl.createPost)
   .get(authCtrl.isLoggedIn, postCtrl.getPosts);
 Router.route("/post/:post_id").get(authCtrl.isLoggedIn, postCtrl.getPosts);
 
